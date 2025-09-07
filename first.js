369b048457f14341edc03bec91d2c5e6d81271b3
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-07 11:01:25 +0600

    Creating Neccessary file html, css, js

---
+++ first.js
@@ -0,0 +1,43 @@
+// console.log("Welcome to my home brother");
+// console.log("Love you Jihad");
+// console.log("I think I am going to love JavaScript");
+
+// age = 24;
+// console.log(age);
+
+// console.log(age + 10);
+
+// fullName = "Tony Stark";
+// console.log(fullName);
+
+// price = 99.99;
+// console.log(price);
+
+// a = null;
+// console.log(a);
+
+// y = undefined;
+// console.log(y);
+
+// isfollow = false;
+// console.log(isfollow);
+
+// isSubscribed = true;
+// console.log(isSubscribed);
+
+// console.log(typeof age)
+// console.log(typeof isfollow)
+
+const student = {
+  name: "Ali hasan",
+  age: 20,
+  cgpa: 8.2,
+  isPassed: true,
+};
+
+console.log(student); 
+
+console.log(student.name);
+
+rose = student["isPassed"];
+console.log(rose)
