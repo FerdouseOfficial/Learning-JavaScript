2372d288754e494bdc254e2c25a1140af8e38194
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-08 22:11:29 +0600

    rating is 4 not 4.3 . that's edited this time okay. Don't be confused what changed or not

--- Practice Question/Pr1/Question 1.js
+++ Practice Question/Pr1/Question 1.js
@@ -3,10 +3,10 @@
 const product = {
   Product_name: "Parker Jotter Standard Ball Pen (Black)",
   Product_Price: 270,
-  Product_Rating: 4.3,
+  Product_Rating: 4,
   Product_Discount: 5 % "off",
 };
 console.log(product);
 console.log(typeof product);
 console.log(product.Product_name);
-console.log(product["Product_Rating"])
+console.log(product["Product_Rating"]);
