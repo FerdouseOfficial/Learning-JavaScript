e5afd3267818409943f6435e8816505bb91615d9
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-08 21:25:53 +0600

    Solving Problem 1

---
+++ Practice Question/Pr1/Question 1.js
@@ -0,0 +1,9 @@
+// Question no 1: Create  a const object called "product" to store information show in the picture
+
+const product = {
+  Product_name: "Parker Jotter Standard Ball Pen (Black)",
+  Product_Price: 270,
+  Product_Rating: 4.3,
+  Product_Discount: 5 % "off",
+};
+console.log(product);
