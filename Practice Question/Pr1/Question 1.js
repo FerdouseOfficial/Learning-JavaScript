2f501cbb7e95bffffd42a09c5c4606034532b187
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-08 22:24:35 +0600

    Solving the 2nd problem

--- Practice Question/Pr1/Question 1.js
+++ Practice Question/Pr1/Question 1.js
@@ -12,3 +12,16 @@ console.log(product.Product_name);
 console.log(product["Product_Rating"]);
 
 // Question no 2: Create a const object called "profile" to store information show in the picture.
+
+const profile = {
+  SubjectName: "ShradhaKhapra",
+  Followers: "569k",
+  Following: 4,
+  Posts: 195,
+  isFollowing: false,
+  isVerfied: true,
+  Bio: "Entrepreneur | Apna College | Ex-Microsoft, DRDO",
+  Pickupline: "To educate someone is the highest privilege",
+};
+
+console.log(profile);
