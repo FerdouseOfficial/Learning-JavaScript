9428f2b457a01e9a7b752b353ab59fef155ed4aa
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-09 09:21:19 +0600

    making Changes a+b= answer.

--- Practice Question/Pr2/Question 2.js
+++ Practice Question/Pr2/Question 2.js
@@ -5,8 +5,8 @@
 let a = 5;
 let b = 2;
 
-console.log(a + b); // Addition
-console.log(a - b); // Subtraction
-console.log(a * b); // Multiplicatoin
-console.log(a / b); // Division
-console.log(a % b); // Modulas
+console.log("a+b =", a + b); // Addition
+console.log("a-b =", a - b); // Subtraction
+console.log("a*b =", a * b); // Multiplicatoin
+console.log("a/b =", a / b); // Division
+console.log("a%b =", a % b); // Modulas
