453867e47172959df3fd882f895d27ceae2c25f0
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-09 09:12:47 +0600

    Practicing the concept of Arithmetic operators

--- Practice Question/Pr2/Question 2.js
+++ Practice Question/Pr2/Question 2.js
@@ -1,8 +1,12 @@
 // Chapter 02: Operator & Conditional Statements
 
-// Arithmetic Opearator:- 
+// Arithmetic Opearator:-
 
 let a = 5;
 let b = 2;
 
-console.log(a+b);
+console.log(a + b); // Addition
+console.log(a - b); // Subtraction
+console.log(a * b); // Multiplicatoin
+console.log(a / b); // Division
+console.log(a % b); // Modulas
