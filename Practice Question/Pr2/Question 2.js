3ffada776e89dd536ff509b96f49c6e9e46d0e74
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-09 09:27:18 +0600

    practicing

--- Practice Question/Pr2/Question 2.js
+++ Practice Question/Pr2/Question 2.js
@@ -5,8 +5,9 @@
 let a = 5;
 let b = 2;
 
-console.log("a+b =", a + b); // Addition
-console.log("a-b =", a - b); // Subtraction
+console.log("a+b =", a + b); // Addition /Increment
+console.log("a-b =", a - b); // Subtraction/ Decrement
 console.log("a*b =", a * b); // Multiplicatoin
 console.log("a/b =", a / b); // Division
-console.log("a%b =", a % b); // Modulas
+console.log("a%b =", a % b); // Modulus
+console.log("a**b =", a**b) // Exponential
