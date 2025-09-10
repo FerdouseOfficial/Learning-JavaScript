936b0a9f68bdf9768c6c012925104b987d9b4d67
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-10 12:00:41 +0600

    Learning and Practing the concept of Loops

---
+++ Practice Question/Pr3/Question 3.js
@@ -0,0 +1,26 @@
+// Question 01
+// Print all even(jora) number from 0 to 100
+
+// Solving
+
+// for(let i=0; i<=100; i++){
+//     if(i%2 ===0){
+//         console.log("num = ",i);
+//     }
+// }
+
+// Question 02
+// Create a game where you start with any random game number.Ask the userto keep guessing the game number until the user enters coreect value.
+
+// MY solution 
+// let User_input = prompt("Guess the Number(0-20) :- ");
+
+// for (let i = 1; i <= 50; i++) {
+//   if (User_input === 4) {
+//     console.log("Congratualtion. You Enter the Correct Value");
+//     alert("Your The Winnder");
+//     break;
+//   } else {
+//     prompt("Guess again");
+//   }
+// }
--- Practice Question/Pr3/practice.js
+++ Practice Question/Pr3/practice.js
@@ -1,1 +1,82 @@
 //  Chapter 03:- Loops & Strings
+
+// We can write for loop in 3 steps:- Initialization, stopping condition, updation
+
+// Print 1 to 5
+// for(let count=1; count <= 5; count++) {
+//     console.log("I am  a Great Programmer");
+// }
+
+// let sum = 0;
+// // let n = prompt("Enter Your Number Please: ");
+// for (let i = 1; i <= n; i++) {
+//   sum = sum + i;
+// }
+// console.log("sum = ", sum);
+
+// ```````         Infinte Loops in JavaScript    '''''''''' //
+
+// infinte Loop: A loop that never Ends
+
+// '''''''''''''' While Loop ''''''''''''''''''//
+
+// 1. we will declare variable outside of the loo
+// 2. Declare the stopping condition inside the parenthisis
+// 3. write the increment property inside the curly brasis after doing some work
+
+// let i = 1;
+
+// while (i <= 5) {
+//   console.log("Apna college");
+//   i++;
+// }
+
+// '''''''''''''''''' DO WHILE LOOP ''''''''''''''''''//
+
+// 1. again declare the variable outside the loop
+// 2. it will do work fast before the condition thats why its name is Do - while loop
+// 3. Write the Do first after writing add the increment(updation) i++;
+
+// 4. Then Write the condition;
+
+// let i = 20;
+
+// do {
+//   console.log("Apna college");
+
+// } while (i <= 10);
+
+// ''''''''''''There are anotehr 2 type of Loops ''''''''''//
+
+// 1. For- of loop
+// 2. For- in loop
+
+// ''''''''''' 1. For-of loop practice //////////
+
+// we use (for-of loop) for strings and arrays not for Object
+
+//  no need to write conditon and updation
+
+// let str = "ApnaCollege";
+// let size = 0;
+// for (let i of str) {
+//   //iterator -> charachter
+//   console.log("i = ", i);
+//   size++;
+// }
+// console.log("String size = ", size);
+
+// ''''''''''''''''''' for-in Loop """''''''''''''" //////
+
+// we use it for object
+
+let student = {
+    Name : 'Mijanur Rahman',
+    age : 20,
+    cgpa: 3.45,
+    isPass: true
+}
+
+for(let key in student) { 
+    console.log("key = ", key, "Value = ", student[key]);
+}
