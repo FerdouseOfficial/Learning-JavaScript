67a723f5e008ea62c43219596ea696f416a65fc5
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-11 08:55:37 +0600

    Apllying loops over an Arrays

--- Practice Question/Pr4/practice4.js
+++ Practice Question/Pr4/practice4.js
@@ -6,11 +6,11 @@
 
 // in array position/index matter. But in Object key matters.In the array the key is the index. Here its in pair or index:Value; like that
 
-let marks = [97, 82, 75, 64, 36];
+// let marks = [97, 82, 75, 64, 36];
 
-console.log(marks);
-console.log(marks.length);
-console.log(typeof marks);
+// console.log(marks);
+// console.log(marks.length);
+// console.log(typeof marks);
 
 // printing value of marks using index.
 
@@ -20,19 +20,48 @@ console.log(typeof marks);
 
 //Changing value of array
 // string is Immutable but array is mutable.
-console.log((marks[2] = 86));
-console.log(marks);
+// console.log((marks[2] = 86));
+// console.log(marks);
 
 //// `````````````````````````` Looping over an Array  ``````````````````````` ////
-//print all element of an array 
+//print all element of an array
 
 // Applying Loops
 
 // loops :- Iterable(string, object, arrays)
-
+// we can also use for -in but its not suggested
 // In array we apply for - of loop . in object we apply for - in loop. remember it
 // for (let val of marks) {
 //   console.log(val);
 // }
 
-let heroes = ["ironman",]
+// let heroes = [
+//   "ironman",
+//   "Hulk",
+//   "Superman",
+//   "Aquaman",
+//   "Spiderman",
+//   "Thor",
+//   "Loki",
+// ];
+
+// for (let i=0; i < heroes.length; i++) {
+//   console.log(heroes[i]);
+// }
+
+// for(let hero of heroes) {
+//     console.log(hero)
+// }
+
+let cities = [
+  "Dhaka",
+  "Ranpur",
+  "Nageswari",
+  "Kurigram",
+  "Lalmonirhat",
+  "Chitagong",
+];
+
+for (let city of cities) {
+  console.log(city);
+}
