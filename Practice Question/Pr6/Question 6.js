017c195bb371f8fab2baf950ab3087895ef4fd54
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-11 19:47:17 +0600

    solving practice questionons

---
+++ Practice Question/Pr6/Question 6.js
@@ -0,0 +1,8 @@
+// Chapter 06: DOM (Document Object Model)
+
+// Problem one SOlved
+let add = document.querySelector("h2");
+
+add.innerText = add.innerText + " from Apna College Studnets";
+
+// Problem 02:- 
--- Practice Question/Pr6/script.js
+++ Practice Question/Pr6/script.js
@@ -15,5 +15,27 @@
 // console.dir(headings);
 // console.log(headings)
 
-let paras = document.getElementsByTagName("p")
-console.log(paras)
+// let paras = document.getElementsByTagName("p")
+// console.log(paras)
+
+
+// in query selector we need to write class and id just like we write in css with dot and # 
+// let Doc = document.querySelector("p");
+// let DocsAll = document.querySelectorAll("p");
+// console.log(Doc);
+// console.log(DocsAll);
+
+
+// when we create a Div node tree :
+
+// we have 3 things :- 3 types of node
+// 1. text node 2. comment nodes 3. elements node
+
+// we alwasy work on element nodes. we have to ignore text node and comment node
+
+// console.log(document.body.firstChild)
+// innerText property only show text
+let div = document.querySelector("div");
+console.log(div)
+
+// innerHTML property show html tags with contents;
