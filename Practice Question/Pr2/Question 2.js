d4699ffcf54ac69fad229db634e860bb56fafdb4
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-11 21:32:06 +0600

    adding tiny changes

---
+++ Practice Question/Pr2/Question 2.js
@@ -0,0 +1,10 @@
+// // Question 02:
+
+// let gameNumber = 12;
+
+// let userInput = prompt("Guess the Number :-");
+// while(gameNumber != userInput) {
+//     userInput = prompt("You entere the wrong number. Guess Again");
+// }
+// console.log("Congratualtion You Win the Match");
+
