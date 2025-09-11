18ffd1cd0e4d9e99930a247383957fa4390a2ff8
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-11 18:54:11 +0600

    L&P about DOM (Document Object Model)

---
+++ Practice Question/Pr6/script.js
@@ -0,0 +1,9 @@
+// Chapter 06: DOM Part-1
+
+// DOM :- (DOCUMENT OBJECT MODEL)
+
+// window Object
+// The windo object represents an open window in a browser. it is browser's object (not JavaScript's) & is automatically created by broswer.
+
+// it is a global object with lot of properties & methods.
+
