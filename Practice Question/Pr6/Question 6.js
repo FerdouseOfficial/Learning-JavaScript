5bb2a0be9f28b6b51594be542ae817912efff8e1
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-11 19:57:14 +0600

    solving problem 02

--- Practice Question/Pr6/Question 6.js
+++ Practice Question/Pr6/Question 6.js
@@ -1,8 +1,14 @@
 // Chapter 06: DOM (Document Object Model)
 
 // Problem one SOlved
-let add = document.querySelector("h2");
+// let add = document.querySelector("h2");
 
-add.innerText = add.innerText + " from Apna College Studnets";
+// add.innerText = add.innerText + " from Apna College Studnets";
 
 // Problem 02:- 
+
+let UniqueText = document.querySelectorAll(".box");
+
+UniqueText[0].innerText = "new Unique text 01";
+UniqueText[1].innerText = "new unique text 02";
+UniqueText[2].innerText = "new unique text 03";
