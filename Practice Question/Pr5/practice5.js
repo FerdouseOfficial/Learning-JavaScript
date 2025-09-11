3bed42e4e00b4d5fb70cb25cd2e16811376b6475
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-11 12:12:43 +0600

    Learning the basic syntax and concept of JS

--- Practice Question/Pr5/practice5.js
+++ Practice Question/Pr5/practice5.js
@@ -1,1 +1,13 @@
 // Chapter 05: Functions
+// block of code that perfroms a specific task, can be invoked(call) whenever needed
+
+// To create Functions we have to follow two step . 1. Funciton Definition & 2. Funciton Call
+
+// function myFunction(msg,n) {
+  // parameter -> input variable
+  // console.log("I am a Unbeatable Super Level Programmer");
+  // console.log("Welcome to Apna College! ");
+// //   console.log(msg + n);
+// }
+// myFunction("I love javascript", 100); // arguement
+// myFunction()
