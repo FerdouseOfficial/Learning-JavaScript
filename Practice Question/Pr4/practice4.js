1deac98eace4e06e8383807ef51aac7a9ddf2499
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-11 08:24:33 +0600

    L&P(Learning and Practicing the concept of

--- Practice Question/Pr4/practice4.js
+++ Practice Question/Pr4/practice4.js
@@ -1,1 +1,20 @@
 // Chapter 04:- Arrays
+
+// arrays - Collection of items :- stored linearly
+
+// array is a special type of Object
+
+// in array position/index matter. But in Object key matters.In the array the key is the index. Here its in pair or index:Value; like that
+
+let marks = [97, 82, 75, 64, 36];
+
+console.log(marks);
+console.log(marks.length);
+console.log(typeof marks);
+
+// Applying Loops
+
+// In array we apply for - of loop . in object we apply for - in loop. remember it 
+for (let val of marks) {
+  console.log(val);
+}
