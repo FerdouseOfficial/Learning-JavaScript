4bfa7da40a08fb7005feb1b7fbcdf5c87191b1a3
FerdouseOfficial <mollajihad402@gmail.com>
2025-09-11 19:07:47 +0600

    L&P about DOM Manipulation. accessing id, class, tag,

--- Practice Question/Pr6/script.js
+++ Practice Question/Pr6/script.js
@@ -7,3 +7,13 @@
 
 // it is a global object with lot of properties & methods.
 
+// let h = document.getElementById("heading");
+// console.log(h);
+
+// let headings = document.getElementsByClassName("heading2-Class");
+
+// console.dir(headings);
+// console.log(headings)
+
+let paras = document.getElementsByTagName("p")
+console.log(paras)
