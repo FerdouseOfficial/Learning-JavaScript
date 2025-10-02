8519865fa11f45e96ace99d89e3ad21f4b285b5f
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-02 12:27:57 +0600

    Practice done of Object and Protypes

--- Practice Question/Pr7/Practice7.js
+++ Practice Question/Pr7/Practice7.js
@@ -1,1 +1,39 @@
 // Classes and Objects
+// const student = {
+//   fullName: "Jihad Molla",
+//   marks: 95.2,
+//   printMarks: function () {
+//     console.log(`marks = ${this.marks}`);
+//   },
+// };
+
+
+
+const employee = {
+  calcTax() {
+    console.log("tax rate is 10%");
+  },
+};
+
+let mijanur = {
+  salary: 30000,
+};
+let mijanur2 = {
+  salary: 30000,
+};
+
+let mijanur3 = {
+  salary: 80000,
+  calcTax() {
+    console.log("Tax rate is 20%");
+  },
+};
+
+let mijanur4 = {
+  salary: 30000,
+};
+
+mijanur.__proto__ = employee;
+mijanur2.__proto__ = employee;
+mijanur3.__proto__ = employee;
+mijanur4.__proto__ = employee;
