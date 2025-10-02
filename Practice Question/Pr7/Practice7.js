78d3bee357cddb04984dfb2bc71f298004e20b6a
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-02 12:38:18 +0600

    stasrt Practicing concepts of Classes

--- Practice Question/Pr7/Practice7.js
+++ Practice Question/Pr7/Practice7.js
@@ -9,31 +9,33 @@
 
 
 
-const employee = {
-  calcTax() {
-    console.log("tax rate is 10%");
-  },
-};
+// const employee = {
+//   calcTax() {
+//     console.log("tax rate is 10%");
+//   },
+// };
 
-let mijanur = {
-  salary: 30000,
-};
-let mijanur2 = {
-  salary: 30000,
-};
+// let mijanur = {
+//   salary: 30000,
+// };
+// let mijanur2 = {
+//   salary: 30000,
+// };
 
-let mijanur3 = {
-  salary: 80000,
-  calcTax() {
-    console.log("Tax rate is 20%");
-  },
-};
+// let mijanur3 = {
+//   salary: 80000,
+//   calcTax() {
+//     console.log("Tax rate is 20%");
+//   },
+// };
 
-let mijanur4 = {
-  salary: 30000,
-};
+// let mijanur4 = {
+//   salary: 30000,
+// };
 
-mijanur.__proto__ = employee;
-mijanur2.__proto__ = employee;
-mijanur3.__proto__ = employee;
-mijanur4.__proto__ = employee;
+// mijanur.__proto__ = employee;
+// mijanur2.__proto__ = employee;
+// mijanur3.__proto__ = employee;
+// mijanur4.__proto__ = employee;
+
+// Practicing concepts of Classes:-
