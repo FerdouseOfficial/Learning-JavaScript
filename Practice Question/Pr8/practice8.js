057defb7a0204ef76be9fc5c5d48a0021838a636
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-07 09:07:16 +0600

    finish of Callback hells

--- Practice Question/Pr8/practice8.js
+++ Practice Question/Pr8/practice8.js
@@ -38,20 +38,23 @@
 //     getData(2)
 // });
 
-function getData(dataId, getNextData) {
-  setTimeout(() => {
-    console.log(`data = ${dataId}`);
-    if (getNextData) {
-      getNextData();
-    }
-  }, 2000);
-}
+// function getData(dataId, getNextData) {
+//   setTimeout(() => {
+//     console.log(`data = ${dataId}`);
+//     if (getNextData) {
+//       getNextData();
+//     }
+//   }, 2000);
+// }
 
-// This is called callback hell
-getData(1, () => {
-  getData(2, () => {
-    getData(3, () => {
-      getData(4);
-    });
-  });
-});
+// // This is called callback hell 
+// getData(1, () => {
+//   getData(2, () => {
+//     getData(3, () => {
+//       getData(4);
+//     });
+//   });
+// });
+
+
+
