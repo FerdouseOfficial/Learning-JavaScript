2b91b4cb571e3b5759e8d52960457f7a854515ca
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-08 19:03:10 +0600

    20% completed

--- Practice Question/Pr9/weather-app/script.js
+++ Practice Question/Pr9/weather-app/script.js
@@ -1,5 +1,11 @@
 const button = document.querySelector("#search-button");
+const input = document.getElementById("cityName");
+async function getData(cityName) {
+  fetch(
+    `http://api.weatherapi.com/v1/current.json?key=8187652ff5d34a8283a123118250810&q=${cityName}&aqi=yes`
+  );
+}
 
 button.addEventListener("click", () => {
-//   document.querySelector("p").innerText = "You click the button";
+  console.log(input.value);
 });
