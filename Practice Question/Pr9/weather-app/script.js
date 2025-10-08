8ec426e1b33ccc65ee45ccbad4606c570e51d113
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-08 19:49:23 +0600

    Learning by doing

--- Practice Question/Pr9/weather-app/script.js
+++ Practice Question/Pr9/weather-app/script.js
@@ -1,11 +1,23 @@
 const button = document.querySelector("#search-button");
-const input = document.getElementById("cityName");
+const input = document.getElementById("cityname");
+
+let cityName = document.getElementById("cityName");
+let cityTime = document.getElementById("cityTime");
+let cityTemp = document.getElementById("cityTemp");
+
 async function getData(cityName) {
-  fetch(
+  let promise = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=8187652ff5d34a8283a123118250810&q=${cityName}&aqi=yes`
   );
+  return await promise.json();
 }
 
-button.addEventListener("click", () => {
-  console.log(input.value);
+button.addEventListener("click", async () => {
+  const value = input.value;
+  const result = await getData(value);
+  cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`;
+  cityTime.innerText = `${result.location.localtime}`;
+  cityTemp.innerText = `${result.current.temp_c}`;
 });
+
+// Also here after wriitng the city name when we press enter in keyboard it will show us the
